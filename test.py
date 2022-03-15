0b41f8337217ed8349d9021a7fc877733e595721
ibrazkn <ibrazkn2000@gmail.com>
2022-03-16 00:37:54 +0300

    Ilk python kodu yayımlandı

---
+++ test.py
@@ -0,0 +1,10 @@
+# Python3 program to add two numbers
+
+num1 = 45
+num2 = 15
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("{0} ve {1} sayısının  toplamı şudur: {2}" .format(num1, num2, sum))
